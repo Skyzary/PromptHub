d1030112e723521d26ca8f0800dd8c6eb2d72d0a
Volodymyr Levkovskyi <132926053+Skyzary@users.noreply.github.com>
2026-05-04 22:27:13 +0200

    Integrate `AddNewBtn` into `Popup` and optimize `SearchField` formatting and structure.

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,14 +1,17 @@
+import AddNewBtn from "~src/components/AddNewBtn/AddNewBtn"
 import Header from "~src/components/Header/Header"
-import SearchField from "~src/components/SearchField/SearchField"
 import ListSection from "~src/components/ListSection/ListSection"
+import SearchField from "~src/components/SearchField/SearchField"
 
-export default function Popup(){
-  return(
-    <div className={'popup'}>
-      <Header/>
-      <SearchField onSearch={(query) => console.log(query)}/>
-      <ListSection/>
+import "./popup.scss"
+
+export default function Popup() {
+  return (
+    <div className={"popup"}>
+      <Header />
+      <SearchField onSearch={(query) => console.log(query)} />
+      <ListSection />
+      <AddNewBtn />
     </div>
-
   )
 }
